ffa9ee62a9b7567d66dc16e073d5740916abb7be
lrovaris <luis.gabrielrovaris@gmail.com>
2021-10-06 23:34:58 -0300

    a

--- routes.js
+++ routes.js
@@ -151,6 +151,7 @@ router.post('/checkValue', (req, res) => {
 router.post ('/mint', async(req,res) => {
 
   for (let i = 0; i < wallet.balance().utxo.length; i++){
+      console.log(wallet.balance().utxo[i].value.lovelace.toString)
     if(wallet.balance().utxo[i].value.lovelace.toString() === req.body.value.toString()){
       if (req.body.value < list[req.body.number - 1]) {
         return res.status(200).json({rs:"not today :3"});
@@ -159,8 +160,10 @@ router.post ('/mint', async(req,res) => {
       quantitysArray[req.body.number] = +quantitysArray[req.body.number] - +1
       res.status(200).json({"message":"check your wallet"})
         console.log(quantitysArray)
+    } else {
+        res.status(200).json({"message":"didn't receive yet"});
     }
-      res.status(200).json({"message":"didn't receive yet"});
+
   }
 })
 
