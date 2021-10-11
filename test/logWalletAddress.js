7fd2e28863010d246575f94e7558412d9b88bc49
lrovaris <luis.gabrielrovaris@gmail.com>
2021-10-11 20:22:55 -0300

    asd

--- test/logWalletAddress.js
+++ test/logWalletAddress.js
@@ -4,7 +4,7 @@ const logWalletAddresses = function () {
 	const wallets = getFakeWallets();
 
 	for (let i = 0; i < wallets.length; i++) {
-		console.log(wallets[i].paymentAddr);
+		console.log(JSON.stringify(wallets[i], null,4));
 	}
 };
 
