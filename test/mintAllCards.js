7aac0731a647f4d2e68b0bfcfe5d34a950df7b4e
lrovaris <luis.gabrielrovaris@gmail.com>
2021-10-15 10:40:11 -0300

    Update mintAllCards.js

--- test/mintAllCards.js
+++ test/mintAllCards.js
@@ -10,16 +10,22 @@ let wallet = cardanocliJs.wallet("testNetWallet");
 
 let i = 0
 let transaction
+let oldQuery = {}
+let newQuery = {}
 
 const verifyTransaction = function () {
 
+       oldQuery = newQuery
+       newQuery = hash(cardanocliJs.queryUtxo(wallet.paymentAddr))
  
+ 
+        if (newQuery !== oldQuery) {
+           console.log("a")
+           transaction = cardanocliJs.transactionSubmit(mintAsset(metadataArray[i], 10000000, cardanocliJs.wallet("fake-wallet-5").paymentAddr))
+           console.log(transaction)  
+        }
 
 
-         transaction = cardanocliJs.transactionSubmit(mintAsset(metadataArray[i], 10000000, cardanocliJs.wallet("fake-wallet-5").paymentAddr))
-         
-         console.log(transaction)  
- 
         isTransactionValid = i >= 50;
 
         i++
