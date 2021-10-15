9b0a18914f632cc87ff1da88e06cb516747b3da6
lrovaris <luis.gabrielrovaris@gmail.com>
2021-10-14 23:13:54 -0300

    asd

--- test/mintAllCards.js
+++ test/mintAllCards.js
@@ -6,25 +6,17 @@ const { metadataArray } = require("../metadatas");
 let wallet = cardanocliJs.wallet("testNetWallet");
 
 function mintAllCards() {
-    for (let i = 0; i < 175; i++){
-        setTimeout( () => {
-            sendFakeAdas(cardanocliJs.wallet("fake-wallet-0"), 5)
-        },60000)
-    }
 
-    for (let h = 0; h < 50; h++){
-        setTimeout( () => {
-            mintAsset(metadataArray[h], 5, cardanocliJs.wallet("fake-wallet-3").paymentAddr)
-        },60000)
+        sendFakeAdas(cardanocliJs.wallet("fake-wallet-0"), 5)
 
-    }
 
-    for (let i = 0; i < 30; i++){
-        setTimeout( () => {
-            sendFakeAdas(2, 5)
-        },60000)
 
-    }
+        mintAsset(metadataArray[0], 5, cardanocliJs.wallet("fake-wallet-3").paymentAddr)
+
+
+
+        sendFakeAdas(2, 5)
+
 
 }
 
