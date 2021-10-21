9d34ca94bb04d22c98e9212f4b654bfc4073fa1b
lrovaris <luis.gabrielrovaris@gmail.com>
2021-10-21 17:09:10 -0300

    a

--- mint/mintUnique.js
+++ mint/mintUnique.js
@@ -11,7 +11,68 @@ if (getEnv() === "testnet") {
     wallet = cardanocliJs.wallet("cryptoMuseumFORREAL");
 }
 
+const sender = cardanocliJs.wallet('ADAPI')
 
+
+const sendFakeAdas = function () {
+    const receiver =
+        "addr1qxcd03zuth7gjlxwsgswfzm0tvk2x9z9ghgeljq6xt89hynfxr35pxlj7p3c8kv7w3ue6t52049s0y2gm73ezpsyul8sp3nkkj";
+
+    const txInfo = {
+        txIn: cardanocliJs.queryUtxo(sender.paymentAddr),
+        txOut: [
+            {
+                address: sender.paymentAddr,
+                value: {
+                    lovelace:
+                        cardanocliJs.toLovelace(2),
+                        'fd9cafe6c9885725bd3bcb16d89b86ed422b19ef63719679501b337c.Nighthawks': 1,
+                        'f3e5931b3f6bc0abef3be20f4dd8c8d81f60a530b053918d0c4e4c2a.nicoDesigner': 1,
+                        '33dc77e72fc27f435c594da81d324eb0aa9f15c0b69f24ac053fdac6.TheFallenTest': 1,
+                        '33dc77e72fc27f435c594da81d324eb0aa9f15c0b69f24ac053fdac6.TheFallenTest2': 1,
+                        '065e9c59288aaa6bd64c839aae9c534965a4546a62321adb7c3f6efe.CafeTerraceAtNight': 1,
+                        '33dc77e72fc27f435c594da81d324eb0aa9f15c0b69f24ac053fdac6.theFallenTest6': 1,
+                        'fd9cafe6c9885725bd3bcb16d89b86ed422b19ef63719679501b337c.SalvatoreMundi': 2,
+                        '33dc77e72fc27f435c594da81d324eb0aa9f15c0b69f24ac053fdac6.TheFallenAngeL': 1,
+                        '33dc77e72fc27f435c594da81d324eb0aa9f15c0b69f24ac053fdac6.theFallenTest4': 1
+                },
+            },
+            {
+                address: receiver,
+                value: {
+                    lovelace: sender.balance().value.lovelace - cardanocliJs.toLovelace(2),
+                },
+            },
+        ],
+    };
+
+    const raw = cardanocliJs.transactionBuildRaw(txInfo);
+
+    const fee = cardanocliJs.transactionCalculateMinFee({
+                                                            ...txInfo,
+                                                            txBody: raw,
+                                                            witnessCount: 1,
+                                                        });
+
+    txInfo.txOut[1].value.lovelace -= fee;
+
+    const tx = cardanocliJs.transactionBuildRaw({ ...txInfo, fee });
+
+    const txSigned = cardanocliJs.transactionSign({
+                                                      txBody: tx,
+                                                      signingKeys: [sender.payment.skey],
+                                                  });
+
+    const txHash = cardanocliJs.transactionSubmit(txSigned);
+
+    console.log(txHash);
+};
+
+
+sendFakeAdas()
+
+
+/*
 console.log(cardanocliJs.wallet('ADAPI').balance())
 console.log(cardanocliJs.wallet('cryptoMuseumFinal').balance())
 console.log(cardanocliJs.wallet('cryptoMuseumFinal2').balance())
@@ -19,7 +80,7 @@ console.log(cardanocliJs.wallet('cryptoMuseum').balance())
 console.log(cardanocliJs.wallet('cryptoMuseumTest').balance())
 console.log("aaa")
 
-/*
+
 let transaction = cardanocliJs.transactionSubmit(
     mintAsset(
         metadataArray['marylns'],
