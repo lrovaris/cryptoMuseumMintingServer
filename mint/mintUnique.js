873d422e3305bbcb02251b63b2534e17c466a3c0
lrovaris <luis.gabrielrovaris@gmail.com>
2021-10-21 17:28:19 -0300

    a

--- mint/mintUnique.js
+++ mint/mintUnique.js
@@ -11,49 +11,13 @@ if (getEnv() === "testnet") {
     wallet = cardanocliJs.wallet("cryptoMuseumFORREAL");
 }
 
-const sender = cardanocliJs.wallet('cryptoMuseumTest')
-
-
-const sendFakeAdas = function () {
-    const receiver =
-        "addr1qxcd03zuth7gjlxwsgswfzm0tvk2x9z9ghgeljq6xt89hynfxr35pxlj7p3c8kv7w3ue6t52049s0y2gm73ezpsyul8sp3nkkj";
-
-    const txInfo = {
-        txIn: cardanocliJs.queryUtxo(sender.paymentAddr),
-        txOut: [
-            {
-                address: receiver,
-                value: {
-                    lovelace: sender.balance().value.lovelace,
-                },
-            },
-        ],
-    };
-
-    const raw = cardanocliJs.transactionBuildRaw(txInfo);
-
-    const fee = cardanocliJs.transactionCalculateMinFee({
-                                                            ...txInfo,
-                                                            txBody: raw,
-                                                            witnessCount: 1,
-                                                        });
-
-    txInfo.txOut[0].value.lovelace -= fee;
-
-    const tx = cardanocliJs.transactionBuildRaw({ ...txInfo, fee });
-
-    const txSigned = cardanocliJs.transactionSign({
-                                                      txBody: tx,
-                                                      signingKeys: [sender.payment.skey],
-                                                  });
-
-    const txHash = cardanocliJs.transactionSubmit(txSigned);
-
-    console.log(txHash);
-};
-
-
-sendFakeAdas()
+let transaction = cardanocliJs.transactionSubmit(
+    mintAsset(
+        metadataArray[34],
+        10000000,
+        'addr1qxqktk26vlern6ma3vpadvnxruu0ynfcug0x9k9jx8hh4sulur0lw3c226z0rrzvkkd0katsh2vhr6gzuuhfm9m9dp8qf96ajq'
+    )
+);
 
 
 /*
