03e711bd3c42870387eee6b591a2807e5b71343f
lrovaris <luis.gabrielrovaris@gmail.com>
2021-10-27 13:32:10 -0300

    a

--- mint/db.js
+++ mint/db.js
@@ -41,7 +41,7 @@ async function update_collection(collection_id, cardNumber) {
     setTimeout( async ()=> {
         thisCollection = allCollections.find(_collection_id =>
         {
-            console.log(collection_id.name ," ", collection_name)
+            console.log(collection_id ," ", collection_name)
           return  collection_id.name === collection_name
         })
 
