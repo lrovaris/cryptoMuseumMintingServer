7a141aa2cad5ebcf8084d5cc107c9ec5994c025c
lrovaris <luis.gabrielrovaris@gmail.com>
2021-11-20 07:28:01 -0300

    aaaaaaaaaaaaaaaa

--- mint/mintUnique.js
+++ mint/mintUnique.js
@@ -23,76 +23,79 @@ const mintScript = {
     type: "sig",
 };
 const POLICY_ID = cardanocliJs.transactionPolicyid(mintScript);
-
-
-
-const x = function (_metadata1, _metadata2) {
-
-    const metadata_1 = {
-        721: {
-            [POLICY_ID]: {
-                [_metadata1.name.replace(/\s/g, "")]: {},
-            },
+const metadata_1 = {
+    721: {
+        [POLICY_ID]: {
+            [_metadata1.name.replace(/\s/g, "")]: {},
         },
-    };
+    },
+};
 
-    metadata_1["721"][`${POLICY_ID}`][_metadata1.name.replace(/\s/g, "")] =
-        _metadata1;
-    const ASSET_ID_1 = `${POLICY_ID}.${_metadata1.name.replace(/\s/g, "")}`;
+metadata_1["721"][`${POLICY_ID}`][_metadata1.name.replace(/\s/g, "")] =
+    _metadata1;
+const ASSET_ID_1 = `${POLICY_ID}.${_metadata1.name.replace(/\s/g, "")}`;
 
 
-    const metadata_2 = {
-        721: {
-            [POLICY_ID]: {
-                [_metadata2.name.replace(/\s/g, "")]: {},
-            },
+const metadata_2 = {
+    721: {
+        [POLICY_ID]: {
+            [_metadata2.name.replace(/\s/g, "")]: {},
         },
-    };
+    },
+};
 
-    metadata_2["721"][`${POLICY_ID}`][_metadata2.name.replace(/\s/g, "")] =
-        _metadata2;
-    const ASSET_ID_2 = `${POLICY_ID}.${_metadata2.name.replace(/\s/g, "")}`;
+metadata_2["721"][`${POLICY_ID}`][_metadata2.name.replace(/\s/g, "")] =
+    _metadata2;
+const ASSET_ID_2 = `${POLICY_ID}.${_metadata2.name.replace(/\s/g, "")}`;
+const x = function () {
 
-    let txInfo = {};
-
-    txInfo = {
+    const receiver = "addr1qy0md3978mwx2z3a8rlywdpknmc655g6tdj2qy3a5veq6qk50ap8s0se070jvgdqzvhsa9nnv3hfyhng7nlf8pdf92tsx4v4af"
+    const txInfo = {
         txIn: cardanocliJs.queryUtxo(sender.paymentAddr),
         txOut: [
             {
                 address: sender.paymentAddr,
                 value: {
-                    lovelace: sender.balance().value.lovelace, [ASSET_ID_1]: 1, [ASSET_ID_2]:1
+                    lovelace: sender.balance().value.lovelace - cardanocliJs.toLovelace(3)
+                }
+            },
+            {
+                address: receiver,
+                value: {
+                    lovelace: cardanocliJs.toLovelace(3), '6654c3dca29414a1404479fe989fae1794623d2c4733ca8891645c7f.WandererAbovetheSeaofFog' :1, '6654c3dca29414a1404479fe989fae1794623d2c4733ca8891645c7f.WomanWithAParassol': 1
                 }
             }
-        ],
-        mint: [
-            { action: "mint", quantity: 1, asset: ASSET_ID_1, script: mintScript },
-            { action: "mint", quantity: 1, asset: ASSET_ID_2, script: mintScript },
-        ],
-        metadata_1,
-        witnessCount: 2
-
+        ]
     }
-    raw = cardanocliJs.transactionBuildRaw(txInfo)
-    fee = cardanocliJs.transactionCalculateMinFee({
+
+    console.log(txInfo);
+
+    const raw = cardanocliJs.transactionBuildRaw(txInfo)
+
+    const fee = cardanocliJs.transactionCalculateMinFee({
         ...txInfo,
         txBody: raw,
-        witnessCount: 3
+        witnessCount: 1
     })
+
     txInfo.txOut[0].value.lovelace -= fee
-    tx = cardanocliJs.transactionBuildRaw({ ...txInfo, fee })
-    txSigned = cardanocliJs.transactionSign({
+
+    const tx = cardanocliJs.transactionBuildRaw({ ...txInfo, fee })
+
+    const txSigned = cardanocliJs.transactionSign({
         txBody: tx,
         signingKeys: [sender.payment.skey]
     })
 
-    txHash = cardanocliJs.transactionSubmit(txSigned)
+    const txHash = cardanocliJs.transactionSubmit(txSigned)
 
-    
+    console.log(txHash)
+
+
 }
 
 
-x(metadataArray[20], metadataArray[39])
+x()
 
 
 
